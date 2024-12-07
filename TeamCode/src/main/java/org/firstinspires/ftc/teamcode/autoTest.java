72238a570c0b32041478db5eae1ebd695ba32549
AlanBrown4 <thebrownalan@gmail.com>
2024-12-07 09:17:38 -0700

    added parking auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class autoTest extends LinearOpMode{
+    // Declare motor objects
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+        // Initialize hardware
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        waitForStart();
+        while(opModeIsActive()){
+            frontLeft.setPower(1.0);
+            backLeft.setPower(1.0);
+            frontRight.setPower(1.0);
+            backRight.setPower(1.0);
+            sleep(1000);
+        }
+    }
+}
